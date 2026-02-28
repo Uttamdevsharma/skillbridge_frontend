17c864917e01209c0237f8f6062ea73c9e28ba22
Uttam <uttamrohit4545@gmail.com>
2026-02-28 22:02:49 +0600

    feat(layout): implement application footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+
 import Link from "next/link";
 import { Facebook, Twitter, Instagram, Linkedin, Mail, Phone, MapPin } from "lucide-react";
 import { usePathname } from "next/navigation";
